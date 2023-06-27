0d6dedc7fd24e46fb9c230637717c868ba60758b
Roseli Mariano <roseli.mariano26@gmail.com>
2023-06-26 21:48:27 -0300

    sass-parte02

---
+++ JS/custon.js
@@ -0,0 +1,18 @@
+
+  $(document).ready(function() {
+    // Abre o modal de contato quando o botão for clicado
+    $('#contatoBtn').click(function() {
+      $('#contatoModal').modal('show');
+    });
+
+    // Abre o modal de serviços quando o botão for clicado
+    $('#servicosBtn').click(function() {
+      $('#servicosModal').modal('show');
+    });
+
+    // Abre o modal de produtos quando o botão for clicado
+    $('#produtosBtn').click(function() {
+      $('#produtosModal').modal('show');
+    });
+  });
+
